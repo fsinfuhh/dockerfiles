ffdfdc71e0c3a6c799f5aea223be8dc76364708a
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2016-04-20 13:01:27 +0200

    unicorn->uwisgi; stammbaum als test

--- build-base.sh
+++ build-base.sh
@@ -1,10 +1,19 @@
 #! /bin/bash
 
+VERSION=0.0.1
+NAME=base-system
+IMAGE_NAME=$NAME-$VERSION-linux-amd64.aci
+echo "Building $IMAGE_NAME"
+
+rm -v $IMAGE_NAME
+
 . acbuildhelper.sh
 
+acbuild set-name rkt.mafiasi.de/$NAME
+debootstrap --variant=minbase jessie .acbuild/currentaci/rootfs http://ftp.de.debian.org/debian/
 
-debootstrap jessie .acbuild/currentaci/rootfs
-acbuild set-name rkt.mafiasi.de/base-system
+acbuild run -- apt update
 acbuild run -- apt install -y vim
-acbuild set-exec -- /bin/bash
-acbuild write base-system.aci
+
+acbuild write $IMAGE_NAME
+sudo -su nils gpg --default-key E4CA7691 --armor --output $IMAGE_NAME.asc --detach-sign $IMAGE_NAME 
