48195d28881d2f03c6ed00479b8868f322f04985
Felix Wiedemann <1wiedema@informatik.uni-hamburg.de>
2016-05-10 10:20:05 +0200

    Gogs

--- gogs/gogs.sh
+++ gogs/gogs.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-VERSION=2016.04.20
+VERSION=2016.05.10
 NAME=gogs
 IMAGE_NAME=$NAME-$VERSION-linux-amd64.aci
 
@@ -15,7 +15,9 @@ acbuild run -- sh -es <<EOF
     adduser --disabled-login --gecos 'Gogs' gogs
     echo deb http://ftp.de.debian.org/debian jessie-backports main >> /etc/apt/sources.list
     apt update
-    apt-get --no-install-recommends -y install golang/jessie-backports git openssl ca-certificates
+    apt-get --no-install-recommends -y install golang/jessie-backports git openssl ca-certificates openssh-server
+    ln -sf /opt/config/sshd_config /etc/ssh/sshd_config
+
     su - gogs
     export GOPATH=~/go
     mkdir go
@@ -26,6 +28,7 @@ acbuild run -- sh -es <<EOF
 EOF
 acbuild port add web tcp 3000
 acbuild port add ssh tcp 22
+acbuild mount add storage /opt/storage
 acbuild set-exec -- /home/gogs/go/src/github.com/gogits/gogs/gogs web
 
 acbuild write --overwrite $IMAGE_NAME
