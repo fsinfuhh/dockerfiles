3b8765ca32847b5516babffc8457a487bbfbaef5
Felix Wiedemann <1wiedema@informatik.uni-hamburg.de>
2016-05-12 16:24:38 +0200

    Gogs

--- gogs/gogs.sh
+++ gogs/gogs.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-VERSION=2016.05.10
+VERSION=2016.05.12
 NAME=gogs
 IMAGE_NAME=$NAME-$VERSION-linux-amd64.aci
 
@@ -11,24 +11,33 @@ echo "Building $IMAGE_NAME"
 acbuild set-name rkt.mafiasi.de/$NAME
 acbuild dependency add rkt.mafiasi.de/base-system
 
-acbuild run -- sh -es <<EOF
-    adduser --disabled-login --gecos 'Gogs' gogs
+acbuild run -- /bin/sh -es <<"EOF"
+    adduser --disabled-login --gecos 'Gogs' --uid 2001 --ingroup nogroup gogs
     echo deb http://ftp.de.debian.org/debian jessie-backports main >> /etc/apt/sources.list
     apt update
-    apt-get --no-install-recommends -y install golang/jessie-backports git openssl ca-certificates openssh-server
-    ln -sf /opt/config/sshd_config /etc/ssh/sshd_config
+    apt-get --no-install-recommends -y install golang/jessie-backports golang-go/jessie-backports golang-src/jessie-backports git openssl ca-certificates openssh-client
 
-    su - gogs
-    export GOPATH=~/go
+    su -c 'sh -s' gogs <<"EOG"
+    cd
+    export GOPATH=$HOME/go
     mkdir go
     go get -u github.com/gogits/gogs
     cd $GOPATH/src/github.com/gogits/gogs
     go build
+EOG
+
+    mkdir -p /home/gogs/go/src/github.com/gogits/gogs/custom/conf
+    ln -sf /opt/config/app.ini /home/gogs/go/src/github.com/gogits/gogs/custom/conf/
     
 EOF
 acbuild port add web tcp 3000
-acbuild port add ssh tcp 22
+acbuild port add ssh tcp 3001
 acbuild mount add storage /opt/storage
+acbuild mount add config /opt/config
+acbuild mount add log /opt/log
+acbuild set-user -- gogs
+acbuild set-group -- gogs
+acbuild set-working-directory -- /home/gogs/go/src/github.com/gogits/gogs
 acbuild set-exec -- /home/gogs/go/src/github.com/gogits/gogs/gogs web
 
 acbuild write --overwrite $IMAGE_NAME
