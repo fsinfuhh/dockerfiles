32ff1f3ef6be7596a30bd71c912987559d5758fa
Felix Wiedemann <1wiedema@informatik.uni-hamburg.de>
2016-05-13 15:15:13 +0200

    Gogs

--- gogs/generate_config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -x
-
-cd "$(dirname "$0")"
-
-ssh-keygen -t rsa -f config/ssh_host_rsa_key
-ssh-keygen -t ed25519 -f config/ssh_host_ed25519_key
--- gogs/gogs.sh
+++ gogs/gogs.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-VERSION=2016.05.12
+VERSION=2016.05.13
 NAME=gogs
 IMAGE_NAME=$NAME-$VERSION-linux-amd64.aci
 
@@ -29,6 +29,13 @@ EOG
     mkdir -p /home/gogs/go/src/github.com/gogits/gogs/custom/conf
     ln -sf /opt/config/app.ini /home/gogs/go/src/github.com/gogits/gogs/custom/conf/
     
+    cat > /usr/local/bin/run <<EOG
+#!/bin/sh
+export USER=gogs HOME=/home/gogs
+exec /home/gogs/go/src/github.com/gogits/gogs/gogs web
+EOG
+    chmod +x /usr/local/bin/run
+
 EOF
 acbuild port add web tcp 3000
 acbuild port add ssh tcp 3001
@@ -36,8 +43,8 @@ acbuild mount add storage /opt/storage
 acbuild mount add config /opt/config
 acbuild mount add log /opt/log
 acbuild set-user -- gogs
-acbuild set-group -- gogs
+acbuild set-group -- nogroup
 acbuild set-working-directory -- /home/gogs/go/src/github.com/gogits/gogs
-acbuild set-exec -- /home/gogs/go/src/github.com/gogits/gogs/gogs web
+acbuild set-exec -- /usr/local/bin/run
 
 acbuild write --overwrite $IMAGE_NAME
