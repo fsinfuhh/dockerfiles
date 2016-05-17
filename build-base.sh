0726712831ddbcfb86d5ec8af591fa565d64f1b5
Felix Wiedemann <1wiedema@informatik.uni-hamburg.de>
2016-05-17 15:35:42 +0200

    Base: Clean up. Include jessie-updates

--- build-base.sh
+++ build-base.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=2016.05.02
+VERSION=2016.05.17
 NAME=base-system
 IMAGE_NAME=$NAME-$VERSION-linux-amd64.aci
 
@@ -11,15 +11,13 @@ acbuild set-name rkt.mafiasi.de/$NAME
 debootstrap --variant=minbase jessie $T/.acbuild/currentaci/rootfs http://ftp.de.debian.org/debian/
 
 acbuild run -- sh -es <<EOF
+echo deb http://ftp.de.debian.org/debian jessie-updates main >> /etc/apt/sources.list
 echo deb http://security.debian.org/ jessie/updates main >> /etc/apt/sources.list
 apt update
 apt upgrade -y
-apt install -y vim unattended-upgrades
-echo 'unattended-upgrades       unattended-upgrades/enable_auto_updates boolean true' | debconf-set-selections
-dpkg-reconfigure -f noninteractive unattended-upgrades
-mkdir /opt/config
+apt install -y vim ca-certificates
+mkdir /opt/config /opt/storage /opt/log
 EOF
 
-acbuild mount add config /opt/config --read-only
 acbuild set-exec -- /bin/bash
 acbuild write --overwrite $IMAGE_NAME
