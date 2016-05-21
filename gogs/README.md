454b24183fc63b088abce4199721348b2108837b
Felix Wiedemann <1wiedema@informatik.uni-hamburg.de>
2016-05-21 16:59:16 +0200

    Gogs

--- gogs/README.md
+++ gogs/README.md
@@ -3,8 +3,8 @@
 UID: 2001
 
 Ports:
-- ssh
-- web
+- ssh: 22
+- web: 3005
 
 Mountpoints:
 - config
@@ -12,15 +12,33 @@ Mountpoints:
 - log
 
 
-Initial steps:
+# Initial steps:
 ```sh
 mkdir -p /opt/config/gogs /var/log/gogs /srv/gogs
 cp $CONFIG_DIR/app.ini /opt/config/gogs/app.ini
-vim !$ # Set DB options
+vim !$ # Set options -- there will be a setup page on first run, too.
 chown 2001:nogroup /opt/config/gogs /opt/config/gogs/app.ini /var/log/gogs /srv/gogs
 ```
 
-How to run:
+The LDAP settings can be configured via the Web interface.
+
+# How to run:
+
+## Option 1: Port 22 of the host will be forwarded to the ssh daemon of the container.
 ```sh
-rkt run --port=web:3000 --port=ssh:22 --volume storage,kind=host,source=/srv/gogs --volume config,kind=host,source=/opt/config/gogs --volume log,kind=host,source=/var/log/gogs --debug --interactive --dns=134.100.9.61 rkt.mafiasi.de/gogs
+rkt run --port=web:3005 --port=ssh:22 --volume storage,kind=host,source=/srv/gogs --volume config,kind=host,source=/opt/config/gogs --volume log,kind=host,source=/var/log/gogs --debug --interactive --dns=134.100.9.61 rkt.mafiasi.de/gogs
+```
+
+## Option 2: The host runs a sshd that will accept ssh connections on port 22 and forwards the data transparently to the container:
+
+Additional setup:
+```sh
+adduser --home $GOGS_STORAGE/gituser --shell $GOGS_STORAGE/gogs-shell --uid 2001 gogs
+su -c "ssh-keygen -t ed25519; cat ~/.ssh/id_ed25519.pub >> ~/.ssh/authorized_keys" gogs
+cp gogs-shell $GOGS_STORAGE/gogs-shell
+```
+
+Run:
+```sh
+rkt run --port=web:3005 --volume storage,kind=host,source=/srv/gogs --volume config,kind=host,source=/opt/config/gogs --volume log,kind=host,source=/var/log/gogs --debug --interactive --dns=134.100.9.61 rkt.mafiasi.de/gogs
 ```
--- gogs/gogs.sh
+++ gogs/gogs.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-VERSION=2016.05.17
+VERSION=2016.05.21
 NAME=gogs
 IMAGE_NAME=$NAME-$VERSION-linux-amd64.aci
 
@@ -15,7 +15,7 @@ acbuild run -- /bin/sh -es <<"EOF"
     adduser --disabled-login --gecos 'Gogs' --uid 2001 --ingroup nogroup gogs
     echo deb http://ftp.de.debian.org/debian jessie-backports main >> /etc/apt/sources.list
     apt update
-    apt-get --no-install-recommends -y install golang/jessie-backports golang-go/jessie-backports golang-src/jessie-backports git openssl ca-certificates openssh-client
+    apt-get --no-install-recommends -y install golang/jessie-backports golang-go/jessie-backports golang-src/jessie-backports git openssl ca-certificates openssh-client openssh-server
 
     su -c 'sh -s' gogs <<"EOG"
     cd
@@ -27,26 +27,27 @@ acbuild run -- /bin/sh -es <<"EOF"
 EOG
 
     mkdir /opt/gogs
-    cp -ra /home/gogs/go/src/github.com/gogits/gogs /opt/gogs
+    cp -ra /home/gogs/go/src/github.com/gogits/gogs /opt
     chown -R root:nogroup /opt/gogs
     mkdir -p /opt/gogs/custom/conf
     ln -sf /opt/config/app.ini /opt/gogs/custom/conf/
+    usermod -d /opt/storage/gituser gogs
+    echo "AcceptEnv SSH_ORIGINAL_COMMAND" >> /etc/ssh/sshd_config
     
     cat > /usr/local/bin/run <<EOG
 #!/bin/sh
+service ssh start
 export USER=gogs HOME=/home/gogs
-exec /opt/gogs/gogs web
+exec su -c 'hostname -I > /opt/storage/gogs.ip; /opt/gogs/gogs web' gogs
 EOG
     chmod +x /usr/local/bin/run
 
 EOF
-acbuild port add web tcp 3000
-acbuild port add ssh tcp 3001
+acbuild port add web tcp 3005
+acbuild port add ssh tcp 22
 acbuild mount add storage /opt/storage
 acbuild mount add config /opt/config
 acbuild mount add log /opt/log
-acbuild set-user -- gogs
-acbuild set-group -- nogroup
 acbuild set-exec -- /usr/local/bin/run
 
 acbuild write --overwrite $IMAGE_NAME
