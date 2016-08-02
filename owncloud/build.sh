4d98b9fbe8f6ee03e1fee3ae99811912d60b8155
Felix Wiedemann <1wiedema@informatik.uni-hamburg.de>
2016-08-02 16:53:19 +0200

    owncloud: Include the exact version string in image name

--- owncloud/build.sh
+++ owncloud/build.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-VERSION=8.2
+V=8.2
+VERSION=$(wget -qO- https://download.owncloud.org/download/repositories/$V/Debian_8.0/Packages  | grep -FxA1 "Package: owncloud" | grep -oE "[0-9]+((-|\.)[0-9]+)*")
 NAME=owncloud
 
 . ../acbuildhelper.sh
@@ -8,12 +9,12 @@ NAME=owncloud
 acbuild set-name rkt.mafiasi.de/$NAME
 acbuild dependency add rkt.mafiasi.de/base
 
-acbuild run -- /usr/bin/env VERSION=$VERSION /bin/sh -es <<"EOF"
+acbuild run -- /usr/bin/env V=$V /bin/sh -es <<"EOF"
     usermod -u 2002 -g nogroup www-data
     apt-get -y --no-install-recommends install wget uwsgi uwsgi-plugin-php php5 php5-gd php5-intl php5-mcrypt php5-pgsql php5-apcu php5-curl php5-memcache php5-redis php5-ldap
 
-    wget -nv https://download.owncloud.org/download/repositories/$VERSION/Debian_8.0/Release.key -O- | apt-key add -
-    echo deb http://download.owncloud.org/download/repositories/$VERSION/Debian_8.0/ / > /etc/apt/sources.list.d/owncloud.list
+    wget -nv https://download.owncloud.org/download/repositories/$V/Debian_8.0/Release.key -O- | apt-key add -
+    echo deb http://download.owncloud.org/download/repositories/$V/Debian_8.0/ / > /etc/apt/sources.list.d/owncloud.list
     apt update
     apt -y install owncloud-files
     apt-get clean
