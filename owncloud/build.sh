a3f0c5222b572bfddb23c6c720acd8d1716476aa
Felix Wiedemann <1wiedema@informatik.uni-hamburg.de>
2017-07-12 10:27:24 +0200

    Upgraded to stretch for owncloud and redis.

--- owncloud/build.sh
+++ owncloud/build.sh
@@ -7,12 +7,12 @@ NAME=owncloud
 . ../acbuildhelper.sh
 
 acbuild set-name rkt.mafiasi.de/$NAME
-acbuild dependency add rkt.mafiasi.de/base
+acbuild dependency add rkt.mafiasi.de/base-stretch
 
 acbuild copy-to-dir hotfix-for-share-links-for-logged-in-users.patch /
 acbuild run -- /usr/bin/env V=$V /bin/sh -es <<"EOF"
     usermod -u 2002 -g nogroup www-data
-    apt-get -y --no-install-recommends install wget php5-fpm php5 php5-gd php5-intl php5-mcrypt php5-pgsql php5-apcu php5-curl php5-memcache php5-redis php5-ldap patch
+    apt-get -y --no-install-recommends install wget php7.0-fpm php7.0 php7.0-gd php7.0-intl php7.0-mcrypt php7.0-pgsql php7.0-apcu php7.0-curl php7.0-memcache php7.0-redis php7.0-ldap php7.0-xml php7.0-zip php7.0-json php7.0-mbstring patch gnupg strace
 
     wget -nv https://download.owncloud.org/download/repositories/$V/Debian_8.0/Release.key -O- | apt-key add -
     echo deb http://download.owncloud.org/download/repositories/$V/Debian_8.0/ / > /etc/apt/sources.list.d/owncloud.list
@@ -22,10 +22,9 @@ acbuild run -- /usr/bin/env V=$V /bin/sh -es <<"EOF"
     cd /var/www/owncloud
     patch -p1 < /hotfix-for-share-links-for-logged-in-users.patch
     sed -i 's/$OC_Channel = '\''stable'\'';/$OC_Channel = '\''foo'\'';/' version.php
-
     ln -sf /opt/config/config.php /var/www/owncloud/config/config.php
-    ln -sf /opt/config/www.conf /etc/php5/fpm/pool.d/www.conf
-    ln -s /var/log/php5-fpm.log /opt/log/php5-fpm.error.log
+    ln -sf /opt/config/www.conf /etc/php/7.0/fpm/pool.d/www.conf
+    ln -s /var/log/php7.0-fpm.log /opt/log/php7.0-fpm.error.log
 
 
     cat > /usr/local/bin/run <<EOG
@@ -33,12 +32,13 @@ acbuild run -- /usr/bin/env V=$V /bin/sh -es <<"EOF"
 export USER=www-data HOME=/home/www-data
 # TODO: this is ugly
 cp -r /var/www/owncloud /opt/static
-exec /usr/sbin/php5-fpm --nodaemonize --fpm-config /etc/php5/fpm/php-fpm.conf
+mkdir /run/php
+exec /usr/sbin/php-fpm7.0 --nodaemonize --fpm-config /etc/php/7.0/fpm/php-fpm.conf
 EOG
     chmod +x /usr/local/bin/run
 
 EOF
-#acbuild copy uwsgi-owncloud.ini /etc/uwsgi/owncloud.ini
+echo '{ "set": ["@rkt/default-whitelist"], "errno": "ENOSYS"}' | acbuild isolator add "os/linux/seccomp-retain-set" -
 acbuild port add http tcp 9787
 acbuild mount add storage /opt/storage
 acbuild mount add config /opt/config
