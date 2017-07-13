7bb6baa02e2e69a2b64e0715c1330a6f95daa7bf
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2017-07-13 20:43:26 +0200

    change dudel2 to github, include submodule, change settings

--- dudel2/build.sh
+++ dudel2/build.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 # Actung unten nochmal weil isso
-LOCAL_PATH=../../opt/Dudel
+#LOCAL_PATH=~nils/git/dudel-django/
 
-GIT_HASH=`cd ${LOCAL_PATH} && git log -n 1 | grep commit | cut -d ' ' -f 2 | cut -b 1-6`
-#$(wget -q --header="Accept: application/vnd.github.v3.sha" -O- https://api.github.com/repos/fsinfuhh/mafiasi/commits/heads/feature_cml | cut -b 1-6)
-VERSION=2017.04.12-$GIT_HASH
+#GIT_HASH=`cd ${LOCAL_PATH} && git log -n 1 | grep commit | cut -d ' ' -f 2 | cut -b 1-6`
+GIT_HASH=$(wget -q --header="Accept: application/vnd.github.v3.sha" -O- https://api.github.com/repos/fsinfuhh/Bitpoll/commits/heads/master | cut -b 1-6)
+VERSION=2017.07.13-$GIT_HASH
 NAME=dudel2
 
 . ../acbuildhelper.sh
@@ -13,26 +13,26 @@ NAME=dudel2
 acbuild set-name rkt.mafiasi.de/$NAME
 acbuild dependency add rkt.mafiasi.de/base
 
-acbuild copy ${LOCAL_PATH} /opt/dudel2/
+#acbuild copy ${LOCAL_PATH} /opt/dudel2/
 acbuild run -- /bin/sh -es <<"EOF"
     usermod -u 2008 -g nogroup -d /opt/dudel2 www-data
     apt-get -y --no-install-recommends install g++ wget uwsgi uwsgi-plugin-python python python-virtualenv python3-pip virtualenv yui-compressor make git python3-psycopg2 python3-ldap3 gettext gcc python3-dev libldap2-dev libsasl2-dev coffeescript ruby-sass
 
     cd /opt
-    #wget -nv https://github.com/fsinfuhh/dudel2/archive/master.tar.gz -O- | tar -xz
-    #mv dudel2-master dudel2
-    rm -rf /opt/dudel2/.pyenv
+    wget -nv https://github.com/fsinfuhh/Bitpoll/archive/master.tar.gz -O- | tar -xz
+    wget -nv https://github.com/fsinfuhh/django-simple-csp/archive/master.tar.gz -O- | tar -xz
+    mv Bitpoll-master dudel2
+    mv django-simple-csp-master/* dudel2/django-simple-csp
+    rm -r django-simple-csp-master
+    #rm -rf /opt/dudel2/.pyenv
     virtualenv --system-site-packages /opt/dudel2/.pyenv -p `which python3`
     cd dudel2
     . .pyenv/bin/activate
     pip3 install -r requirements.txt
     pip3 install -U 'Django<1.12'
-    pip3 install django-ldapdb
     pip3 install django-auth-ldap
     pip3 install uwsgi
     pip3 install raven
-    #make
-    mv dudel/settings.py.example dudel/settings.py
     ln -sf /opt/static/ /opt/dudel2/_static
     #./manage.py compilestatic
     #./manage.py collectstatic --noinput --link
@@ -40,7 +40,7 @@ acbuild run -- /bin/sh -es <<"EOF"
     chown 2008 -R _static
     chmod o+r -R .
 
-    ln -sf /opt/config/settings.py /opt/dudel2/dudel/settings.py
+    ln -sf /opt/config/settings.py /opt/dudel2/bitpoll/settings_local.py
     ln -sf /opt/storage/media /opt/dudel2/_media
     apt-get -y purge yui-compressor git python-pip make gcc python-dev libldap2-dev libsasl2-dev
     apt-get -y autoremove
