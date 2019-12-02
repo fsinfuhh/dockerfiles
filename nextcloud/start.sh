323e5a5a5aa9211cc3d1531cf603765b2c2dba0c
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2019-12-02 14:11:34 +0100

    nextcloud fixes

--- nextcloud/start.sh
+++ nextcloud/start.sh
@@ -3,4 +3,6 @@ export USER=www-data HOME=/home/www-data
 # TODO: this is ugly
 cp -r /var/www/nextcloud /opt/static
 mkdir /run/php
+chown www-data /opt/config/
+chown www-data /opt/log
 exec /usr/sbin/php-fpm7.3 --nodaemonize --fpm-config /etc/php/7.3/fpm/php-fpm.conf
