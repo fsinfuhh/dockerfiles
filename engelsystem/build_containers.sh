c324087d3704bd5522ccf763ce160e66b243a511
kritzl <kritzl@kritzl.dev>
2023-09-01 22:07:33 +0200

    update build of engelsystem

--- engelsystem/build_containers.sh
+++ engelsystem/build_containers.sh
@@ -4,6 +4,5 @@ set +x
 
 D=$(realpath $(dirname $0))
 
-podman build "$D/engelsystem" -f "$D/engelsystem/docker/Dockerfile" -t registry.mafiasi.de/engelsystem-php
-podman build "$D/engelsystem" -f "$D/engelsystem/docker/nginx/Dockerfile" -t registry.mafiasi.de/engelsystem-nginx
+podman build "$D/engelsystem" -f "$D/engelsystem/docker/Dockerfile" -t registry.mafiasi.de/engelsystem
 
