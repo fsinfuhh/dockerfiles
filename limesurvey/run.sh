6f3bc29b13e6e427dd18f30f1345ea6aa5c832d2
Timon Engelke <timon.engelke@online.de>
2023-09-19 14:25:46 +0200

    Update php version for limesurvey

--- limesurvey/run.sh
+++ limesurvey/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Ugly, I know...
 chown -R www-data /app/upload /var/www/limesurvey/tmp
-/usr/sbin/php-fpm7.4 --daemonize --fpm-config /etc/php/7.4/fpm/php-fpm.conf
+/usr/sbin/php-fpm8.2 --daemonize --fpm-config /etc/php/8.2/fpm/php-fpm.conf
 exec nginx -g 'daemon off;'
