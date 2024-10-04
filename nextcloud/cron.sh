54bfb1ee03327729725697951c366cafaef8f6b8
ftsell <dev@ftsell.de>
2024-10-04 22:03:51 +0200

    add custom cron file to nextcloud image

---
+++ nextcloud/cron.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+set -e
+
+echo "Preparing environment"
+cp /opt/config/config.php /var/www/nextcloud/config/config.php
+chown -R www-data /var/www/nextcloud/config /var/www/nextcloud/apps /opt/log
+chmod +x ./occ
+mkdir -p /run/php
+export USER=www-data HOME=/home/www-data
+
+echo "Syncing system address books with dav"
+su -s /var/www/nextcloud/occ www-data dav:sync-system-addressbook
+
+echo "Running cron.php"
+su -s /usr/bin/php www-data /var/www/nextcloud/cron.php
+
