3bb035d1b1ecf27d7f9df0417958b3504c777cc9
ftsell <dev@ftsell.de>
2024-10-07 16:22:41 +0200

    add background-worker.sh script for nextcloud

---
+++ nextcloud/background-worker.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+cp /opt/config/config.php /var/www/nextcloud/config/config.php
+chown -R www-data /var/www/nextcloud/config /var/www/nextcloud/apps /opt/log
+chmod +x /var/www/nextcloud/occ
+export USER=www-data HOME=/home/www-data
+
+exec su -s /var/www/nextcloud/occ background-jobs:worker
