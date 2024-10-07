1fb3b764efe30d19dba442567d67a606e03b7f4c
ftsell <dev@ftsell.de>
2024-10-07 21:14:48 +0200

    differentiate between long and quick cron scripts in nextcloud

---
+++ nextcloud/longcron.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+set -e
+
+echo "Preparing environment"
+cp /opt/config/config.php /var/www/nextcloud/config/config.php
+chown -R www-data /var/www/nextcloud/config /var/www/nextcloud/apps /opt/log
+chmod +x /var/www/nextcloud/occ
+mkdir -p /run/php
+export USER=www-data HOME=/home/www-data
+
+echo "Syncing system address books with dav"
+su -s /var/www/nextcloud/occ www-data -- dav:sync-system-addressbook
+
+echo "Add missing db indices"
+su -s /var/www/nextcloud/occ www-data -- db:add-missing-indices
+
+echo "Running expensive maintenance repair tasks"
+su -s /var/www/nextcloud/occ www-data -- maintenance:repair --include-expensive
+
--- nextcloud/quickcron.sh
+++ nextcloud/quickcron.sh
@@ -8,15 +8,6 @@ chmod +x /var/www/nextcloud/occ
 mkdir -p /run/php
 export USER=www-data HOME=/home/www-data
 
-echo "Syncing system address books with dav"
-su -s /var/www/nextcloud/occ www-data -- dav:sync-system-addressbook
-
 echo "Running cron.php"
 su -s /usr/bin/php www-data /var/www/nextcloud/cron.php
-
-echo "Add missing db indices"
-su -s /var/www/nextcloud/occ www-data -- db:add-missing-indices
-
-echo "Running expensive maintenance repair tasks"
-su -s /var/www/nextcloud/occ www-data -- maintenance:repair --include-expensive
 
