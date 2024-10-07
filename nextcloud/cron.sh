1d4cd1f60db2c5631492e675fb6afa507c2f29b4
ftsell <dev@ftsell.de>
2024-10-07 16:25:27 +0200

    fix path in cron.sh for nextcloud image

--- nextcloud/cron.sh
+++ nextcloud/cron.sh
@@ -4,7 +4,7 @@ set -e
 echo "Preparing environment"
 cp /opt/config/config.php /var/www/nextcloud/config/config.php
 chown -R www-data /var/www/nextcloud/config /var/www/nextcloud/apps /opt/log
-chmod +x ./occ
+chmod +x /var/www/nextcloud/occ
 mkdir -p /run/php
 export USER=www-data HOME=/home/www-data
 
