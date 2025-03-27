cf77e624d15619ec5e400d24b25959a029e5ae13
Jona Zantz <20zantz@cloak.mafiasi.de>
2025-03-27 11:07:26 +0100

    Change tag for Engelsystem

--- engelsystem/build_containers.sh
+++ engelsystem/build_containers.sh
@@ -4,5 +4,5 @@ set +x
 
 D=$(realpath $(dirname $0))
 
-podman build "$D/engelsystem" -f "$D/engelsystem/docker/Dockerfile" -t registry.mafiasi.de/engelsystem
+podman build "$D/engelsystem" -f "$D/engelsystem/docker/Dockerfile" -t git.mafiasi.de/server-ag/dockerfiles/engelsystem
 
