b8991cd004b2c11ed5c0069867c421d2b57cd300
Timon Engelke <timon.engelke@online.de>
2025-04-07 23:25:05 +0200

    fix indico dockerfile

--- indico/run.sh
+++ indico/run.sh
@@ -5,7 +5,9 @@ set -x
 mkdir -p /app/log/nginx
 nginx
 
-indico db upgrade
+# Ignore indico db upgrade failures for now -- this should not stay this way!
+
+indico db upgrade || true
 indico db --all-plugins upgrade
 
 # run worker as nobody
