2b3a7dcadbb36501c7d3e787ee6899150db5edc6
Timon Engelke <timon.engelke@online.de>
2025-04-09 20:30:47 +0200

    update indico again

--- indico/run.sh
+++ indico/run.sh
@@ -5,9 +5,7 @@ set -x
 mkdir -p /app/log/nginx
 nginx
 
-# Ignore indico db upgrade failures for now -- this should not stay this way!
-
-indico db upgrade || true
+indico db upgrade
 indico db --all-plugins upgrade
 
 # run worker as nobody
